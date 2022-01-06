72c8bfd4c345eeef139d7a959e51cab929695dfc
agussolari <agustinraigoso@hotmail.com>
2022-01-06 15:09:46 -0300

    Test

---
+++ backend/stack.c
@@ -0,0 +1,7 @@
+//
+// Created by agussolari on 6/1/22.
+//
+
+#include "stack.h"
+
+// Maquina de estado que estara activa cuando el tetrino este cayendo
---
+++ backend/stack.h
@@ -0,0 +1,8 @@
+//
+// Created by agussolari on 6/1/22.
+//
+
+#ifndef TETRIS_PROGRA_1_STACK_H
+#define TETRIS_PROGRA_1_STACK_H
+
+#endif //TETRIS_PROGRA_1_STACK_H
