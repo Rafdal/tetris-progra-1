92f502a56681c97933a98257877beb8f6058d96f
Rafael Dalzotto <rdalzotto@itba.edu.ar>
2022-01-08 16:17:21 -0300

    rpi

--- libs/joystick.h
+++ libs/joystick.h
@@ -5,8 +5,19 @@
  *      JOYSTICK TO DPAD RASPBERRY  *
  ************************************/
 
+#include <stdint.h>
+
+typedef void (*dpad_callback_t)(uint8_t);
+
+#define DPAD_LONG_PRESS 500 // milisegundos
+#define DPAD_UMBRAL 70 // umbral de activacion (joystick)
+
+enum {DPAD_UP, DPAD_RIGHT, DPAD_DOWN, DPAD_LEFT, DPAD_UPLEFT, DPAD_UPRIGHT};
 
 void dpad_init(void);
 void dpad_run(void);
+
+void dpad_on_press(dpad_callback_t callback); // Setear callback click de tecla (joystick)
+void dpad_on_longpress(dpad_callback_t callback); // Setear callback para Long Press
 
 #endif // JOYSTICK_H
--- main_rasp.c
+++ main_rasp.c
@@ -3,14 +3,46 @@
 #include "./libs/easy_timer.h"
 #include "./libs/joystick.h"
 
+void key_press_callback(uint8_t key_id){
+    switch (key_id)
+    {
+    case DPAD_UP:
+        printf("UP\n");
+        break;
+
+    case DPAD_DOWN:
+        printf("DOWN\n");
+        break;
+
+    case DPAD_LEFT:
+        printf("LEFT\n");
+        break;
+
+    case DPAD_RIGHT:
+        printf("RIGHT\n");
+        break;
+
+    case DPAD_UPRIGHT:
+        printf("UPRIGHT\n");
+        break;
+
+    case DPAD_UPLEFT:
+        printf("UPLEFT\n");
+        break;
+
+    default:
+        break;
+    }
+}
+
 int main(void){
 
     dpad_init();
-    interval_t inter = set_interval(dpad_run, 30);
+    dpad_on_press(key_press_callback);
 
     while (1)
     {
-        run_interval(&inter);
+        dpad_run();
     }
 
     return 0;
