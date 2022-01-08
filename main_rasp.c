8f1a03a7d14b18cfca520df3bcd80817e606a226
Rafael Dalzotto <rdalzotto@itba.edu.ar>
2022-01-08 14:46:49 -0300

    rpi

--- main_rasp.c
+++ main_rasp.c
@@ -4,21 +4,27 @@
 #include "./libs/joydrv.h"
 #include "./libs/easy_timer.h"
 
+jcoord_t pos, lastPos;
+
 void callback(void){
     joy_update();
     jcoord_t pos = joy_get_coord();
-
-    char buf[32];
-    sprintf(buf, "coord: %d,%d\n", pos.x, pos.y);
-    printf(buf);
-    for(int i=0; i< strlen(buf); i++){
-        putchar((char)8);
+    if(pos.x != lastPos.x || pos.y != lastPos.y){
+        char buf[32];
+        sprintf(buf, "coord: %d,%d\n\0", pos.x, pos.y);
+        printf(buf);
+        for(int i=0; i< strlen(buf); i++){
+            putchar((char)8);
+        }
+        lastPos = pos;
     }
+
 }
+
 
 int main(void){
 
-    interval_t inter = set_interval(callback, 500);
+    interval_t inter = set_interval(callback, 200);
     joy_init();
 
     while (1)
