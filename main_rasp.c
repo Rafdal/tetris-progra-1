02bdb64b82f12203e951d8cf8118d32db09886fb
Rafael Dalzotto <rdalzotto@itba.edu.ar>
2022-01-08 14:32:58 -0300

    rpi

--- main_rasp.c
+++ main_rasp.c
@@ -2,13 +2,24 @@
 
 
 #include "./libs/joydrv.h"
+#include "./libs/easy_timer.h"
 
-int main(void){
-    joy_init();
+void callback(void){
     joy_update();
     jcoord_t pos = joy_get_coord();
 
     printf("coord: %d,%d\n", pos.x, pos.y);
+}
+
+int main(void){
+
+    interval_t inter = set_interval(callback, 500);
+    joy_init();
+
+    while (1)
+    {
+        run_interval(&inter);
+    }
 
     return 0;
 }
