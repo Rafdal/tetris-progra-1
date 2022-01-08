c883efce0c0618c8004cdf0972b7a800552a573d
Rafael Dalzotto <rdalzotto@itba.edu.ar>
2022-01-08 13:55:56 -0300

    rpi

--- main_rasp.c
+++ main_rasp.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 
 #include "./libs/easy_timer.h"
-#include "./front_rpi/joystick.h"
+#include "./libs/joystick.h"
 
+void callback(void){
+    printf("callback\n");
+}
 
 int main(void){
+    printf("Funciona\n");
+
+    interval_t inter = set_interval(callback, 1000);
+
+    while (1)
+    {
+        run_interval(&inter);
+    }    
 
     return 0;
 }
