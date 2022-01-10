98b0b872d0eeff4974ba2ae0e18bb49881e40ffc
Rafael Dalzotto <rdalzotto@itba.edu.ar>
2022-01-10 00:06:31 -0300

    JOYSTICK funciona muy bien!

--- libs/joystick.c
+++ libs/joystick.c
@@ -3,7 +3,7 @@
 #include "joystick.h"
 #include "joydrv.h"
 
-extern const char dpad_key_names[][DPAD_NAMELEN]= {"UP", "RIGHT", "DOWN", "LEFT", "UPLEFT", "UPRIGHT", "BTN", "idle"};
+const char dpad_key_names[][DPAD_NAMELEN] = {"UP", "RIGHT", "DOWN", "LEFT", "UPLEFT", "UPRIGHT", "BTN", "idle"};
 
 //  #############################
 //  #     PRIVATE VARIABLES     #
