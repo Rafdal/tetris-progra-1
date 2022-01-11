3e37fd69e640746b496c8a53471962a007615c82
Rafael Dalzotto <rdalzotto@itba.edu.ar>
2022-01-10 21:06:13 -0300

    Testing integration

--- libs/rpi_display.c
+++ libs/rpi_display.c
@@ -22,6 +22,7 @@ void rpi_set_display(uint8_t y, uint8_t x, uint8_t val){
 
 void rpi_copyToDis (matrix_hand_t fromMat, uint8_t y_offset, uint8_t x_offset)
 {
+	assert(mat_validate(&fromMat, 16, 10));
 	mat_copyFromTo(&fromMat, &matrix, y_offset, x_offset);
 }
 
