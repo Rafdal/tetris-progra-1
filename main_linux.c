c0474d06f2b7ab92a7959c0f3cbe881fbb79a39f
Rafael Dalzotto <rafaeldalzotto2016@gmail.com>
2022-01-29 13:39:16 -0300

    linux"

--- main_linux.c
+++ main_linux.c
@@ -32,34 +32,35 @@
     al_play_sample(sample, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_LOOP, NULL);
 }
 */
-    char matriz [16][10]={
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,1,1,1,1,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,5,5,5,0,0,0},
-        {0,0,0,0,0,5,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-        {0,0,0,0,0,0,0,0,0,0},
-    };
+char matriz [16][10]={
+    {0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,1,1,1,1,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,5,5,5,0,0,0},
+    {0,0,0,0,0,5,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0},
+    {0,0,0,0,0,0,0,0,0,0},
+};
 
 
-    bool close_display = false;
+bool close_display = false;
 
 int initialize_display(void);
-void endgame (void);
+void end_program (void);
 void update_display(void);
 
 void keypress_callback(uint8_t key);
 
+void read_events(void);
 
 int main (void){
 
@@ -75,42 +76,32 @@ int main (void){
     keyb_use_press_callback_for_longpress(KEYB_LEFT);
     keyb_use_press_callback_for_longpress(KEYB_RIGHT);
 
-    game_data_t game_data;
-    while (!close_display && (game_data = game_get_data()).state != GAME_QUIT) {
-
-        ALLEGRO_EVENT ev;
-        if (al_get_next_event(event_queue, &ev)) //Toma un evento de la cola, VER RETURN EN DOCUMENT.
-        {
-            if(keyb_run(&ev)){
-                printf("ESCAPE fue presionado\n");
-            }
-
-            if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE){
-                close_display = true;
-            }
-        }
-
-		if(game_data.state == GAME_RUN && get_millis()-lastMillis >= game_data.speed_interval){
-			if(game_data.id == 0)
-				game_insert_block(id_next_block[0]);
-			else
-				game_move_down();
-			game_run();
-			update_display();
-			lastMillis = get_millis();
-		}
-
-
-		if(game_data.state == GAME_LOSE){
-			printf("Perdiste! The Game\n");
-			game_init();
-		}
-    }
-    endgame();//borro todo
+    // 
+    
+    menu_run(main_menu);
+    
+    
+    end_program();//borro todo
 
     return 0;
 
 }
+
+
+
+void read_events(void){
+    ALLEGRO_EVENT ev;
+    if (al_get_next_event(event_queue, &ev)) //Toma un evento de la cola, VER RETURN EN DOCUMENT.
+    {
+        keyb_run(&ev);
+
+        if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE){
+            close_display = true;
+        }
+    }
+}
+
+
 
 void keypress_callback(uint8_t key){
     switch (key)
@@ -279,7 +270,7 @@ int initialize_display(void) {
     return 0;
 }
 
-void endgame (void){
+void end_program (void){
     al_destroy_display(display);
     al_destroy_bitmap(image);
     al_destroy_bitmap(muroH);
