a9c6e4f7335cd6553cb7fc74ee5e54cdf3f412f3
Agustin <Agus C>
2022-01-29 14:16:30 -0300

    make

---
+++ frontend/TextBlocks.c
@@ -0,0 +1,6 @@
+#include <allegro5/allegro.h> // NO OLVIDAR AGREGAR EN EL LINKER DEL PROYECTO
+#include <allegro5/allegro_image.h> //NO OLVIDAR INCLUIR ALLEGRO_IMAGE EN LINKER
+#include <allegro5/allegro_audio.h> // NO OLVIDAR AGREGAR EN EL LINKER DEL PROYECTO
+#include <allegro5/allegro_acodec.h> // NO OLVIDAR AGREGAR EN EL LINKER DEL PROYECTO
+#include <allegro5/allegro_font.h>
+
