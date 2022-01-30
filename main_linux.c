6a7b60023518ff044b696800a73ce9dad1d19f9d
Agustin <Agus C>
2022-01-30 13:25:13 -0300

    anda la animacion de eliminar filas

--- main_linux.c
+++ main_linux.c
@@ -29,7 +29,7 @@
     ALLEGRO_BITMAP *muroV = NULL;
     ALLEGRO_SAMPLE *sample = NULL;
     ALLEGRO_EVENT_QUEUE * event_queue = NULL;
-
+    ALLEGRO_BITMAP *pieza_blanca = NULL;
 /*void playAudio(void){
     al_play_sample(sample, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_LOOP, NULL);
 }
@@ -64,13 +64,13 @@ void keypress_callback(uint8_t key);
 void read_events(void);
 void update_menu_display(void);
 void animation_row_compleate(void);
+void main_game_start(void);
 //int init_audio(void);
 
 
 int main (void){
 
 	game_init();
-	uint64_t lastMillis;
 	int ret= initialize_display();
 //	int init_audio();
     if(ret){
@@ -84,12 +84,44 @@ int main (void){
 
     // 
    // menu_run(main_menu);
+   main_game_start();
     
     end_program();//borro todo
 
     return 0;
 
 }
+void main_game_start(void){
+
+	game_init();
+
+    game_data_t game_data;
+    uint64_t lastMillis;
+
+    game_start();
+
+    while ((game_data = game_get_data()).state != GAME_QUIT)
+    {
+        read_events();
+        
+        
+        if(game_data.state == GAME_RUN && get_millis()-lastMillis >= game_data.speed_interval){
+            game_move_down();
+            game_run();
+			animation_row_compleate();
+            update_display();
+            lastMillis = get_millis();
+        }
+
+        if(game_data.state == GAME_LOSE){
+            printf("Perdiste! The Game\n");
+            break;
+            #warning BREAK HARDCODEADO
+        }
+    }
+    printf("Leaving game...\n");
+}
+
 
 void animation_row_compleate(void)
 {
@@ -108,21 +140,21 @@ void animation_row_compleate(void)
             for(z=1; z<=ANCHO; z++){
                 al_draw_scaled_bitmap(image, 0, 0, (al_get_bitmap_width(image)/8), al_get_bitmap_height(image), BLOCKSZ*z, BLOCKSZ*(row_compleate[i]), BLOCKSZ, BLOCKSZ, 0);
               //pongo el fondo en negro
-                al_draw_tinted_scaled_rotated_bitmap_region(image, 0, 0, al_get_bitmap_width(image)/8, al_get_bitmap_height(image), al_map_rgba_f(1, 1, 1, 1),(al_get_bitmap_width(image)/8)/2,al_get_bitmap_height(image)/2, (BLOCKSZ/2 +BLOCKSZ*z), (BLOCKSZ/2 +BLOCKSZ*(row_compleate[i])),reductor, reductor, angulo, 0);
+                al_draw_tinted_scaled_rotated_bitmap(pieza_blanca,  al_map_rgba_f(1, 1, 1, 1), al_get_bitmap_width(pieza_blanca)/2, al_get_bitmap_height(pieza_blanca)/2, (BLOCKSZ/2 +BLOCKSZ*z), (BLOCKSZ/2 +BLOCKSZ*(row_compleate[i])),reductor, reductor, angulo, 0);
                 //se va haciendo mas chia a medida que rota
                 al_flip_display();
-                al_rest(0.007);
+                al_rest(0.003);
                 
             }
             reductor-=decremento;
         }
            for(z=1; z<=ANCHO; z++){
-                al_draw_scaled_bitmap(image,(al_get_bitmap_width(image)/8), 0, (al_get_bitmap_width(image)/8), al_get_bitmap_height(image), BLOCKSZ*z, BLOCKSZ*5, BLOCKSZ, BLOCKSZ, 0);
+                al_draw_scaled_bitmap(image,(al_get_bitmap_width(image)/8), 0, (al_get_bitmap_width(image)/8), al_get_bitmap_height(image), BLOCKSZ*z, BLOCKSZ*(row_compleate[i]), BLOCKSZ, BLOCKSZ, 0);
                 al_flip_display();
            } //pongo el fondo en negro de nuevo
 
-		delete_row(row_compleate[i]);
 		row_compleate[i]= 0;
+		delete_row(row_compleate[i]);
 
 	}
 }
@@ -143,7 +175,8 @@ void read_events(void){
         keyb_run(&ev);
 
         if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE){
-            close_display = true;
+            game_quit();
+           
         }
     }
 }
@@ -190,6 +223,12 @@ void keypress_callback(uint8_t key){
             printf("BTN\n");
             break;
 
+        case KEYB_ESC:
+            game_quit();
+            printf("Game quit\n");
+            break;
+            
+
         default:
             break;
     }
@@ -232,6 +271,85 @@ int initialize_display(void) {
 
 
      ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+   /////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    if (!al_init_image_addon()) { // ADDON necesario para manejo(no olvidar el freno de mano) de imagenes 
+        printf("failed to initialize image addon !\n");
+        return -1;
+    }
+
+    image = al_load_bitmap("./frontend/images/piezastetris.png");
+    if (!image) {
+       printf( "failed to load image !\n");
+        al_destroy_event_queue(event_queue);
+        return -1;
+    }
+
+     muroH = al_load_bitmap("./frontend/images/muroH.jpg");
+    if (!muroH) {
+        printf( "failed to load muroH !\n");
+        al_destroy_bitmap(image);
+        al_destroy_event_queue(event_queue);
+        return -1;
+    }
+     muroV = al_load_bitmap("./frontend/images/muroV.jpg");
+    if (!muroV) {
+        printf("failed to load muroV !\n");
+        al_destroy_bitmap(image);
+        al_destroy_bitmap(muroH);
+        al_destroy_event_queue(event_queue);
+        return -1;
+    }
+     pieza_blanca = al_load_bitmap("./frontend/images/white_tile.png");
+    if (!pieza_blanca) {
+        printf("failed to load pieza_blanca !\n");
+        al_destroy_bitmap(image);
+        al_destroy_bitmap(muroH);
+        al_destroy_bitmap(muroV);
+        al_destroy_event_queue(event_queue);
+        return -1;
+    }
+
+    display = al_create_display(((ANCHO+2)*BLOCKSZ), ((ALTO+1)*BLOCKSZ));
+    if (!display) {
+        al_destroy_bitmap(image);
+        al_destroy_bitmap(muroH);
+        al_destroy_bitmap(muroV);
+        al_destroy_event_queue(event_queue);
+        fprintf(stderr,"failed to create display!\n");
+        return -1;
+    }
+    al_register_event_source(event_queue, al_get_display_event_source(display));
+    //ahora dibujo el muro horizontal
+    al_draw_scaled_bitmap(muroH, 0, 0, al_get_bitmap_width(muroH), al_get_bitmap_height(muroH), 0, BLOCKSZ*(ALTO), al_get_display_width(display), BLOCKSZ, 0);
+    //empiezo uso la ultima fila
+    //dibujo las paredes
+    al_draw_scaled_bitmap(muroV, 0, 0, al_get_bitmap_width(muroV), al_get_bitmap_height(muroV), 0, 0, BLOCKSZ, al_get_display_height(display)-BLOCKSZ, 0);
+    al_draw_scaled_bitmap(muroV, 0, 0, al_get_bitmap_width(muroV), al_get_bitmap_height(muroV),al_get_display_width(display)-BLOCKSZ , 0, BLOCKSZ, al_get_display_height(display)-BLOCKSZ, 0);
+    
+    /* al_draw_scaled_bitmap(image, (al_get_bitmap_width(image)/8)*2, 0, (al_get_bitmap_width(image)/8), al_get_bitmap_height(image), BLOCKSZ-5, 0, BLOCKSZ, BLOCKSZ, 0);
+        al_draw_scaled_bitmap(image, (al_get_bitmap_width(image)/8), 0, (al_get_bitmap_width(image)/8), al_get_bitmap_height(image), BLOCKSZ*2, 0, BLOCKSZ, BLOCKSZ, 0);
+            al_draw_scaled_bitmap(image, (al_get_bitmap_width(image)/8)*2, 0, (al_get_bitmap_width(image)/8), al_get_bitmap_height(image), BLOCKSZ*3-4, 0, BLOCKSZ, BLOCKSZ, 0);
+
+
+    al_draw_tinted_scaled_bitmap(image, al_map_rgba_f(0.2, 0.2, 0.2, 0.2), (al_get_bitmap_width(image)/8)*2, 0, (al_get_bitmap_width(image)/8), al_get_bitmap_height(image), BLOCKSZ*2, BLOCKSZ, BLOCKSZ, BLOCKSZ, 0);
+    //void al_draw_bitmap(ALLEGRO_BITMAP *bitmap, float dx, float dy, int flags) 
+    al_draw_scaled_bitmap(image,
+            0, 0, al_get_bitmap_width(image), al_get_bitmap_height(image), //imagen
+            0, 0, al_get_display_width(display), al_get_display_height(display), //a que tamaño queres que se dibuje la imagen
+            0); */ //SIn flags podrian usar ALLEGRO_FLIP_HORIZONTAL o ALLEGRO_FLIP_VERTICAL muy utiles
+    
+    
+    al_flip_display();
+    al_rest(2);
+    
+    
+    //playAudio();
+
+    return 0;
+}
      int init_audio(void) {
     ALLEGRO_DISPLAY *display = NULL;
     ALLEGRO_SAMPLE *sample = NULL;
@@ -284,75 +402,6 @@ int initialize_display(void) {
     al_destroy_display(display);
     al_destroy_event_queue(event_queue);
     al_destroy_sample(sample);
-    return 0;
-}
-   /////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-    if (!al_init_image_addon()) { // ADDON necesario para manejo(no olvidar el freno de mano) de imagenes 
-        printf("failed to initialize image addon !\n");
-        return -1;
-    }
-
-    image = al_load_bitmap("./frontend/images/piezastetris.png");
-    if (!image) {
-       printf( "failed to load image !\n");
-        al_destroy_event_queue(event_queue);
-        return -1;
-    }
-
-     muroH = al_load_bitmap("./frontend/images/muroH.jpg");
-    if (!muroH) {
-        printf( "failed to load image !\n");
-        al_destroy_bitmap(image);
-        al_destroy_event_queue(event_queue);
-        return -1;
-    }
-     muroV = al_load_bitmap("./frontend/images/muroV.jpg");
-    if (!muroV) {
-        printf("failed to load image !\n");
-        al_destroy_bitmap(image);
-        al_destroy_bitmap(muroH);
-        al_destroy_event_queue(event_queue);
-        return -1;
-    }
-
-    display = al_create_display(((ANCHO+2)*BLOCKSZ), ((ALTO+1)*BLOCKSZ));
-    if (!display) {
-        al_destroy_bitmap(image);
-        al_destroy_bitmap(muroH);
-        al_destroy_bitmap(muroV);
-        al_destroy_event_queue(event_queue);
-        fprintf(stderr,"failed to create display!\n");
-        return -1;
-    }
-    al_register_event_source(event_queue, al_get_display_event_source(display));
-    //ahora dibujo el muro horizontal
-    al_draw_scaled_bitmap(muroH, 0, 0, al_get_bitmap_width(muroH), al_get_bitmap_height(muroH), 0, BLOCKSZ*(ALTO), al_get_display_width(display), BLOCKSZ, 0);
-    //empiezo uso la ultima fila
-    //dibujo las paredes
-    al_draw_scaled_bitmap(muroV, 0, 0, al_get_bitmap_width(muroV), al_get_bitmap_height(muroV), 0, 0, BLOCKSZ, al_get_display_height(display)-BLOCKSZ, 0);
-    al_draw_scaled_bitmap(muroV, 0, 0, al_get_bitmap_width(muroV), al_get_bitmap_height(muroV),al_get_display_width(display)-BLOCKSZ , 0, BLOCKSZ, al_get_display_height(display)-BLOCKSZ, 0);
-    
-    /* al_draw_scaled_bitmap(image, (al_get_bitmap_width(image)/8)*2, 0, (al_get_bitmap_width(image)/8), al_get_bitmap_height(image), BLOCKSZ-5, 0, BLOCKSZ, BLOCKSZ, 0);
-        al_draw_scaled_bitmap(image, (al_get_bitmap_width(image)/8), 0, (al_get_bitmap_width(image)/8), al_get_bitmap_height(image), BLOCKSZ*2, 0, BLOCKSZ, BLOCKSZ, 0);
-            al_draw_scaled_bitmap(image, (al_get_bitmap_width(image)/8)*2, 0, (al_get_bitmap_width(image)/8), al_get_bitmap_height(image), BLOCKSZ*3-4, 0, BLOCKSZ, BLOCKSZ, 0);
-
-
-    al_draw_tinted_scaled_bitmap(image, al_map_rgba_f(0.2, 0.2, 0.2, 0.2), (al_get_bitmap_width(image)/8)*2, 0, (al_get_bitmap_width(image)/8), al_get_bitmap_height(image), BLOCKSZ*2, BLOCKSZ, BLOCKSZ, BLOCKSZ, 0);
-    //void al_draw_bitmap(ALLEGRO_BITMAP *bitmap, float dx, float dy, int flags) 
-    al_draw_scaled_bitmap(image,
-            0, 0, al_get_bitmap_width(image), al_get_bitmap_height(image), //imagen
-            0, 0, al_get_display_width(display), al_get_display_height(display), //a que tamaño queres que se dibuje la imagen
-            0); */ //SIn flags podrian usar ALLEGRO_FLIP_HORIZONTAL o ALLEGRO_FLIP_VERTICAL muy utiles
-    
-    
-    al_flip_display();
-    al_rest(2);
-    
-    
-    //playAudio();
-
     return 0;
 }
 
