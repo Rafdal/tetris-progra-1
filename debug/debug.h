19510349c799ee9b7ae823f8780c273f6c5f21c0
Rafael Dalzotto <rafaeldalzotto2016@gmail.com>
2022-02-01 20:38:15 -0300

    debug lib

--- debug/debug.h
+++ debug/debug.h
@@ -5,11 +5,11 @@
 #include <stdarg.h>
 
 
-int debug_new_file(char* _filename);
-void deb_set_file_name(char* _filename);
+int debug_new_file(char* _filename); // Crear un nuevo archivo en blanco o borrar uno existente
+void deb_set_file_name(char* _filename); // Setear nombre de archivo
 
-int deb_println(const char *__format, ...);
+int deb_println(const char *__format, ...); // Printf con salto de linea
 
-int deb_print(const char *__format, ...);
+int deb_print(const char *__format, ...); // Printf sin salto de linea
 
 #endif
